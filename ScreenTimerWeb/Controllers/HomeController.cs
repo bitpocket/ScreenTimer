5e49217e4bb1411d8297e8631abf783c9c1f25e2
Cendrzak, Wojciech (FP) <wcendrzak@future-processing.com>
2015-11-16 20:43:25 +0100

    customify

--- ScreenTimerWeb/Controllers/HomeController.cs
+++ ScreenTimerWeb/Controllers/HomeController.cs
@@ -15,14 +15,8 @@ namespace ScreenTimerWeb.Controllers
 
 		public ActionResult About()
 		{
-			ViewBag.Message = "Your application description page.";
-
-			return View();
-		}
-
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
+			ViewBag.Message = "ScreenTime manager.";
+			ViewBag.Version = "Version 0.0.1 (initial)";
 
 			return View();
 		}
