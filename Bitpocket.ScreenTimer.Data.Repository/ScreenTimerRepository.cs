23dcc9aa1dc5f95d903d4201d3a8cc08d8ddb815
Cendrzak, Wojciech (FP) <wcendrzak@future-processing.com>
2015-11-17 02:40:23 +0100

    added files

---
+++ Bitpocket.ScreenTimer.Data.Repository/ScreenTimerRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using Bitpocket.ScreenTimer.Data;
+using Bitpocket.ScreenTimer.Data.Repository;
+using System.Linq.Expressions;
+
+namespace Bitpocket.ScreenTimer.Test
+{
+	public class ScreenTimerRepository : IRepository
+	{
+		private screentimer_dbEntitiesContext _context;
+
+		public ScreenTimerRepository()
+		{
+			_context = new screentimer_dbEntitiesContext();
+		}
+
+		public IQueryable<T> All<T>() where T : class
+		{
+			return _context.Set<T>();
+		}
+
+		public void Dispose()
+		{
+			if (_context != null) _context.Dispose();
+		}
+
+		public IQueryable<T> AllIncluding<T>(params Expression<Func<T, object>>[] include) where T : class
+		{
+			IQueryable<T> result = _context.Set<T>();
+
+			return result;
+		}
+
+		public IQueryable<T> GetUser<T>(string name) where T : class
+		{
+			IQueryable<T> result = _context.Set<T>();
+			return result;
+		}
+	}
+}
---
+++ Bitpocket.ScreenTimer.Test/ConsumerTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bitpocket.ScreenTimer.Data;
+using Bitpocket.ScreenTimer.Entity;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Bitpocket.ScreenTimer.Test
+{
+	[TestClass]
+	public class ConsumerTests
+	{
+		private IRepository _repository;
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			_repository = new ScreenTimerRepository();
+		}
+
+		[TestMethod]
+		public void Query_AllUsers_NoException()
+		{
+			var list = _repository.All<AspNetUser>();
+
+			foreach (var item in list)
+			{
+				Trace.TraceInformation("User Name: {0}", item.UserName);
+			}
+		}
+
+		[TestMethod]
+		public void GetSpecificUserId()
+		{
+			string name = "a@b.c";
+
+			var list = _repository.AllIncluding<AspNetUser>()
+				.Where<AspNetUser>(x => x.UserName == name);
+
+			foreach (var item in list)
+			{
+				Trace.TraceInformation("User Name: {0}", item.Id);
+			}
+		}
+
+		[TestMethod]
+		public void NoUserForFailedName()
+		{
+			string name = "someFailedName";
+
+			var list = _repository.AllIncluding<AspNetUser>()
+				.Where<AspNetUser>(x => x.UserName == name);
+
+			foreach (var item in list)
+			{
+				throw new Exception("should return no value");
+			}
+		}
+	}
+}
